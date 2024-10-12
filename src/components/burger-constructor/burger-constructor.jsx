a77c5f7334bc820dbdc4201aa5f69e30c204812f
Maxim Sinyukov <verero1173@yandex.ru>
2024-10-12 17:45:45 +0300

    feat: add default burger-constructor styles. Task #5

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -49,51 +49,77 @@ const BurgerConstructor = React.memo(function BurgerConstructor() {
   return (
     <section
     className={`mt-25 ${burgerConstructorStyle['burger-constructor']}`}>
-      <div
-      className={`pl-8 ${burgerConstructorStyle['burger-constructor__ingredients']}`}>
-        {ingredientsData.buns[0]
-          &&  (
-                <ConstructorElement
-                type="top"
-                isLocked={true}
-                text={ingredientsData.buns[0].name + ' (верх)'}
-                price={ingredientsData.buns[0].price}
-                thumbnail={ingredientsData.buns[0].image_mobile}/>
-              )
-        }
+      {
+        ingredientsData.buns.length === 0 && ingredientsData.otherIngredients.length === 0
+          ? (
+            <p
+            className={`text text_type_main-default ${burgerConstructorStyle['burger-constructor__empty-constructor']}`}>
+              Кажется чего-то не хватает...
 
-        <ul
-        className={`${burgerConstructorStyle['burger-constructor__main-ingredients']}`}>
-          {
-            ingredientsData.otherIngredients
-              .map((ingredient) => (
-                <li
-                key={'order-ingredient-' + ingredient._id}
-                className={burgerConstructorStyle['burger-constructor__ingredient']}>
-                  <DragIcon
-                  className="mr-2"
-                  type="primary"/>
+              <br/>
 
-                  <ConstructorElement
-                  text={ingredient.name}
-                  price={ingredient.price}
-                  thumbnail={ingredient.image_mobile}/>
-                </li>
-              ))
-          }
-        </ul>
+              Добавим ингредиентов?&#128064;
+            </p>
+          ) : (
+            <div
+              className={`pl-8 ${burgerConstructorStyle['burger-constructor__ingredients']}`}>
+                {ingredientsData.buns[0]
+                  &&  (
+                        <ConstructorElement
+                        type="top"
+                        isLocked={true}
+                        text={ingredientsData.buns[0].name + ' (верх)'}
+                        price={ingredientsData.buns[0].price}
+                        thumbnail={ingredientsData.buns[0].image_mobile}
+                        extraClass={`${burgerConstructorStyle['burger-constructor__buns']}`}/>
+                      )
+                }
 
-        {ingredientsData.buns[0]
-          &&  (
-                <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                text={ingredientsData.buns[0].name + ' (низ)'}
-                price={ingredientsData.buns[0].price}
-                thumbnail={ingredientsData.buns[0].image_mobile}/>
-              )
-        }
-      </div>
+                <ul
+                className={`${burgerConstructorStyle['burger-constructor__main-ingredients']}`}>
+                  {
+                    ingredientsData.otherIngredients.length > 0
+                      ? ingredientsData.otherIngredients
+                          .map((ingredient) => (
+                            <li
+                            key={'order-ingredient-' + ingredient._id}
+                            className={burgerConstructorStyle['burger-constructor__ingredient']}>
+                              <DragIcon
+                              className="mr-2"
+                              type="primary"/>
+
+                              <ConstructorElement
+                              text={ingredient.name}
+                              price={ingredient.price}
+                              thumbnail={ingredient.image_mobile}/>
+                            </li>
+                          )) : (
+                            <p
+                            className={`text text_type_main-default ${burgerConstructorStyle['burger-constructor__empty-other-ingredients']}`}>
+                              Уже очень вкусно!&#128171;
+
+                              <br/>
+
+                              Добавим еще чего-нибудь?
+                            </p>
+                          )
+                  }
+                </ul>
+
+                {ingredientsData.buns[0]
+                  &&  (
+                        <ConstructorElement
+                        type="bottom"
+                        isLocked={true}
+                        text={ingredientsData.buns[0].name + ' (низ)'}
+                        price={ingredientsData.buns[0].price}
+                        thumbnail={ingredientsData.buns[0].image_mobile}
+                        extraClass={`${burgerConstructorStyle['burger-constructor__buns']}`}/>
+                      )
+                }
+              </div>
+          )
+      }
 
       <div
       className={`mt-10 pr-4 ${burgerConstructorStyle['burger-constructor__order-info']}`}>
